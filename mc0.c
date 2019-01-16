c13ce5992f7c942dc39b18c9d06e677e8da5af1b
sb-why <33081805+sb-why@users.noreply.github.com>
2019-01-16 13:04:13 -0500

    Add files via upload

--- mc0.c
+++ mc0.c
@@ -1,1 +1,10 @@
+#include <iostream>
+#include <stdio.h>
 
+int main(int argc, char** argv) {
+  printf("===== Mid-Day Commander, v0 ===== \n");
+  printf("G’day, Commander! What command would you like to run? \n");
+  printf("0. whoami : Prints out the result of the whoamicommand\n");
+  printf("1. last : Prints out the result of the last command \n");
+  printf("2. ls : Prints out the result of a listing on a user-specified path)\n");
+}
